a68624f4055931bacfabd8d83571562fa62063e5
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-07-21 00:24:29 +0900

    til error resume

--- algoritm/백준/골드5/백준_1461_도서관.py
+++ algoritm/백준/골드5/백준_1461_도서관.py
@@ -16,50 +16,43 @@ def book_move(minus_box,end) :
         for i in range(len(minus_box)):
             if i == 0:
                 ans -= minus_box[i]
-                count += 1
             elif count % n == 0:
                 ans -= minus_box[i] * 2
-                count += 1
-            else:
-                count += 1
-
+            count += 1
 
     else:
         for i in range(len(minus_box)):
             if count % n == 0:
                 ans -= minus_box[i] * 2
-                count += 1
-            else:
-                count += 1
+            count += 1
+
     return ans
 
-
-
-
-plus_point = 0
-for i in range(m):
-    if box[i] >=0:
-        plus_point = i
-        break
-
-minus_box = box[0: plus_point]
-plus_box= box[plus_point:]
+minus_box = []
+plus_box = []
+for i in range(len(box)):
+  if box[i]>0:
+    plus_box.append(box[i])
+  else:
+    minus_box.append(box[i])
 # print(minus_box)
 # print(plus_box)
 
-ans = 0
-if abs(minus_box[0]) > abs(plus_box[-1]) :
+# 박스세팅,ans 초기화
+for i in range(len(plus_box)):
+    plus_box[i] = plus_box[i] * -1
+plus_box = sorted(plus_box)
 
-    for i in range(len(plus_box)):
-        plus_box[i] = plus_box[i] * -1
-    plus_box =  sorted(plus_box)
+# 걸음계산
+ans = 0
+if len(minus_box) == 0:
+    ans += book_move(plus_box,1)
+elif len(plus_box) == 0:
+    ans += book_move(minus_box,1)
+elif abs(minus_box[0]) > abs(plus_box[0]) :
     ans += book_move(plus_box,0)
     ans += book_move(minus_box,1)
-
 else:
-    for i in range(len(plus_box)):
-        plus_box[i] = plus_box[i] * -1
-    plus_box = sorted(plus_box)
     ans += book_move(plus_box,1)
     ans += book_move(minus_box,0)
 

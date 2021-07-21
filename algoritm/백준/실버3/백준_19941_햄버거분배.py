b53252a370425ab43def23cc1f6aa1a46baf825c
DESKTOP-KVCQHCD\multicampus <devysi0827@gmail.com>
2021-07-22 00:05:30 +0900

    til 0721 err resume

--- algoritm/백준/실버3/백준_19941_햄버거분배.py
+++ algoritm/백준/실버3/백준_19941_햄버거분배.py
@@ -3,31 +3,20 @@ sys.stdin = open("1.txt", "r")
 
 n, m = list(map(int,input().split()))
 strings = list(input())
-print(n,m)
-print(strings)
 
 ans = 0
 
 for i in range(len(strings)):
-    flag = 0
     if strings[i] == 'P':
-        for j in range(i-1,i-m-1,-1):
-            if j < 0:
-                break
+        for j in range(i-m,i+m+1):
+            if j < 0 or j >= n:
+                continue
             if strings[j] == 'H':
                 strings[j] = 'X'
                 ans += 1
                 flag = 1
                 break
-        if flag == 1:
-            continue
-        for j in range(i + 1, i + m +1):
-            if j >= n:
-                break
-            if strings[j] == 'H':
-                strings[j] = 'X'
-                ans += 1
-                continue
+
 print(ans)
 
 
